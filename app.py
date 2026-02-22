aebc7fc2fa27d448518476d878afb3bf7983a09a
urvashishrivastava24-ux <urvashishrivastava24@gmail.com>
2026-02-22 21:26:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,26 @@ if st.button("Predict"):
         st.success("Result: Pass 🎉")
     else:
         st.error("Result: Fail ❌")
+
+import streamlit as st
+import numpy as np
+import joblib
+
+pipeline = joblib.load("student_pipeline.pkl")
+
+st.title("Student Performance Prediction")
+
+studytime = st.number_input("Study Time")
+absences = st.number_input("Absences")
+failures = st.number_input("Failures")
+
+if st.button("Predict"):
+    
+    features = np.array([[studytime, absences, failures]])
+    
+    prediction = pipeline.predict(features)
+
+    if prediction[0] == 1:
+        st.success("Pass")
+    else:
+        st.error("Fail")
